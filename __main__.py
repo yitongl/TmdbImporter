ab4ec96995517d8cfc1144e5605e1d0e759149fd
Yitong Liu <yitongl@users.noreply.github.com>
2017-12-15 21:11:50 +0100

    Add database module

--- __main__.py
+++ __main__.py
@@ -2,11 +2,18 @@
 import os
 import tmdbsimple as tmdb
 import lib.tmdb_importer as importer
+from lib.db import DB
 
 def main():
     """ Import all movies from TMDB """
     tmdb.API_KEY = os.environ['TMDB_API_KEY']
+    db_username = os.environ['DB_USERNAME']
+    db_password = os.environ['DB_PASSWORD']
+    db_name = os.environ['DB_NAME']
+    db = DB(db_username, db_password, db_name)
     movie = importer.get_movie_info(623)
+    db.insert_movie(movie)
+
     print(movie)
 
 if __name__ == "__main__":
--- lib/db.py
+++ lib/db.py
@@ -1,2 +1,41 @@
-class DB:
-    pass
+""" Database module """
+import psycopg2
+
+class DB(object):
+    """ Database connector """
+    def __init__(self, username, password, database):
+        # Connect to an existing database
+        self.conn = psycopg2.connect(database=database, user=username, password=password)
+
+    def __get_or_create_genres(self, cur, genres):
+        ids  = []
+        for genre in genres:
+            cur.execute("SELECT id FROM genres WHERE name = %s;", (genre, ))
+            id = cur.fetchone()
+            if not id:
+                cur.execute("INSERT into genres (name) VALUES (%s);", (genre, ))
+                self.conn.commit()
+                cur.execute("SELECT id FROM genres WHERE name = %s;", (genre, ))
+                id = cur.fetchone()
+
+            ids.append(id[0])
+
+        return ids
+
+    def __get_or_create_persons(self, cur, persons):
+        # TODO: Similar as genres
+        pass
+
+    def insert_movie(self, movie):
+        cur = self.conn.cursor()
+
+        genres = self.__get_or_create_genres(cur, movie['genres'])
+        cast = self.__get_or_create_persons(cur, movie['cast'])
+        directors = self.__get_or_create_persons(cur, movie['directors'])
+        # TODO: Insert movie and create relations to genres and persons
+
+
+        cur.close()
+
+    def close(self):
+        self.conn.close()
